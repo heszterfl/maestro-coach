045d119d55cec29e9471272570741ae04d1e8bfc
Eszter Éva Havasi <havasiesztereva@gmail.com>
2026-01-05 10:10:40 +0100

    chore: add missing Spring Boot project files

---
+++ src/main/java/com/maestrocoach/maestro_coach/MaestroCoachApplication.java
@@ -0,0 +1,13 @@
+package com.maestrocoach.maestro_coach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaestroCoachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaestroCoachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maestrocoach/maestro_coach/MaestroCoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maestrocoach.maestro_coach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaestroCoachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
